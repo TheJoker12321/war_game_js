b7603bf4b218e496fa2205b1f8f0c2be2deb1729
Idan <idansano10@gmail.com>
2025-12-07 15:13:21 +0200

    fixed string

--- utils/deck.js
+++ utils/deck.js
@@ -1,4 +1,4 @@
-function createCard (rank, suite) {
+function createCard(rank, suite) {
     const specialCards = {
         "A": 14,
         "J": 11,
@@ -6,14 +6,14 @@ function createCard (rank, suite) {
         "K": 13
     }
 
-        return {
-            "rank": rank,
-            "suite": suite,
-            "value": rank in specialCards ? specialCards[rank] : Number(rank)
-        }
+    return {
+        "rank": rank,
+        "suite": suite,
+        "value": rank in specialCards ? specialCards[rank] : Number(rank)
+    }
 }
 
-function compareCard (p1_card, p2_card) {
+function compareCard(p1_card, p2_card) {
     if (p1_card.value > p2_card.value) {
         return "p1"
     } else if (p1_card.value < p2_card.value) {
@@ -23,7 +23,7 @@ function compareCard (p1_card, p2_card) {
 }
 
 
-function createDeck () {
+function createDeck() {
     const specialCards = {
         11: "J",
         12: "Q",
@@ -32,14 +32,14 @@ function createDeck () {
     }
     const deck = []
     const suites = ["H", "S", "D", "C"]
-    for (let i = 2; i <= 14; i ++) {
-        for (const suite of suites) {    
-            deck.push(createCard(i > 10 ? specialCards[i] : i, suite))
+    for (let i = 2; i <= 14; i++) {
+        for (const suite of suites) {
+            deck.push(createCard(i > 10 ? specialCards[i] : String(i), suite))
         }
     }
     return deck
 }
-function shuffle (deck) {
+function shuffle(deck) {
     let count = 0
     while (count < 1000) {
         let idx1 = Math.floor(Math.random() * 52)
@@ -50,7 +50,7 @@ function shuffle (deck) {
         let temp = deck[idx1]
         deck[idx1] = deck[idx2]
         deck[idx2] = temp
-        count ++
+        count++
     }
     return deck
 }
@@ -61,3 +61,4 @@ export {
     createDeck,
     shuffle
 }
+console.log(shuffle(createDeck()));
